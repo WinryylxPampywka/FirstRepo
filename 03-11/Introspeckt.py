67da0b579efa3d3725ab2c7b1ddf99bc5cacbf4a
First Project <a4749693@gmail.com>
2024-11-03 09:57:43 +0200

    age

---
+++ 03-11/Introspeckt.py
@@ -0,0 +1,23 @@
+import requests
+
+def first_func():
+    pass
+
+class Human:
+    pass
+
+rs = requests
+
+first_f = first_func
+ilya = Human
+
+print(type(rs))
+print(type(first_f))
+print(type(ilya))
+
+print(requests.__name__)
+print(rs.__name__)
+print(first_f.__name__)
+print(ilya.__name__)
+
+print(__name__)
