a191857e7c056f53ec282c4a00e8e2e9aabfd515
First Project <a4749693@gmail.com>
2024-11-10 09:06:29 +0200

    Homework

--- HOMEWORK/Animal.py
+++ HOMEWORK/Animal.py
@@ -1,12 +1,16 @@
 class Animal:
-    def __init__(self,name,weight,quantity):
+    def __init__(self,name,weight,quantity,age,height):
         self.name = name
         self.weight = weight
         self.quantity = quantity
+        self.age = age
+        self.height = height
 
 
         def printAnimal(self):
             print(f'Name: {self.ns.name}')
             print(f'Weight: {self.ns.weight}')
             print(f'Quantity: {self.ns.quantity}')
-    print(f'Name: Cat, Weight: 8 kg,Quantity: 1 ')
+            print(f'Age: {self.ns.Age}')
+            print(f'Height: {self.ns.height}')
+    print(f'Name: Cat, Weight: 8 kg,Quantity: 1, Age: 8 years: Height: 34 см ')
