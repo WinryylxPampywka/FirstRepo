af76042a93eba4e44ea2ea05e9ec65a6f2d77679
First Project <a4749693@gmail.com>
2024-11-10 10:17:07 +0200

    Homework

--- 10-11/Logger1.py
+++ 10-11/Logger1.py
@@ -1,10 +1,21 @@
 import logging
-logging.basicConfig(level=logging.DEBUG,filename="logs.log")
-print("-----------------")
+logging.basicConfig(
+    level=logging.
+    DEBUG,filename="logs.log",
+    filemode='a',
+    format="We have next logging message: %(asctime)s:%(levelname) s -  %(message)s"
+)
 
 
-logging.debug("debug")
-logging.info("info")
-logging.warning("warning")
-logging.error("error")
-logging.critical("critical")
+
+# logging.debug("debug2")
+# logging.info("info2")
+# logging.warning("warning2")
+# logging.error("error2")
+# logging.critical("critical2")
+# print("-----------------")
+
+try:
+     print(10/0)
+except Exception:
+ logging.exception("Exception")
