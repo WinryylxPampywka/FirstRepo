4615631758a0b643dc77f0dc4b2b2915b5b65f21
First Project <a4749693@gmail.com>
2024-11-10 09:02:40 +0200

    Homework

--- 27.10.py
+++ 27.10.py
@@ -30,7 +30,22 @@ except Exception as error:
 #first_student.printStudent()
 #print(f'after creating Student object  {first_student.student_amout}')
 
-print("Next code")
+#print("Next code")
 #firs_student.Birthday()
+
+
+
+class Animal:
+    def __init__(self,name,weight,quantity):
+        self.name = name
+        self.weight = weight
+        self.quantity = quantity
+
+
+        def printAnimal(self):
+            print(f'Name: {self.ns.name}')
+            print(f'Weight: {self.ns.weight}')
+            print(f'Quantity: {self.ns.quantity}')
+    print(f'Name: Cat, Weight: 8 kg,Quantity: 1 ')
 
 
---
+++ HOMEWORK/Animal.py
@@ -0,0 +1,12 @@
+class Animal:
+    def __init__(self,name,weight,quantity):
+        self.name = name
+        self.weight = weight
+        self.quantity = quantity
+
+
+        def printAnimal(self):
+            print(f'Name: {self.ns.name}')
+            print(f'Weight: {self.ns.weight}')
+            print(f'Quantity: {self.ns.quantity}')
+    print(f'Name: Cat, Weight: 8 kg,Quantity: 1 ')
